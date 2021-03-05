cfe5d50604625ab263307780a3a9d5dd7654f4e5
aaronfeingold <ajfeingold88@gmail.com>
2021-03-04 23:11:57 -0600

    changed up the routing and port

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :meals
+
+      resources :users do 
+        resources :meals
+      end
+
       post '/login', to: 'auth#create'
-        get '/current_user', to: 'auth#show'
-        post '/sign_up', to: 'users#create'
+      get '/current_user', to: 'auth#show'
+      post '/sign_up', to: 'users#create'
     end 
   end
   
